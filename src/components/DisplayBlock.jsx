e60c669ae458b96b0ff6083217ea5d3dae297db6
alexsouza10 <alexsouza160298@gmail.com>
2023-11-15 12:18:56 -0300

    feat: StarFull_add_in_DisplayBlock

--- src/components/DisplayBlock.jsx
+++ src/components/DisplayBlock.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cellbike from "/images/cellbike.jpeg"
-import { Container, Title, SubTitle, ContentContainer, ImageLeft } from "../style/DisplayBlockStyle";
-import FaRegStar from '../../public/icons/star'
+import { Container, ContentContainer, Title, SubTitle, ContainerUser , ImageLeft } from "../style/DisplayBlockStyle";
+import StarFull from '/public/icons/StarFull.jsx'
 
 
 const Display = () => {
@@ -15,7 +15,9 @@ const Display = () => {
                     Build high-quality and user-friendly mobile applications for iOS and Android with our team of skilled mobile software engineers
                 </SubTitle>
             </ContentContainer>
-            <FaRegStar /> <FaRegStar /> <FaRegStar /> <FaRegStar /><FaRegStar />
+            <ContainerUser>
+            <StarFull />
+            </ContainerUser>
             <ImageLeft src={cellbike} />
         </Container>
     )
