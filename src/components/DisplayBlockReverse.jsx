608313c7fdce7987020e14acb59a1a5536440e09
alexsouza10 <alexsouza160298@gmail.com>
2023-11-17 07:54:17 -0300

    feat: margin_on_DisplayBlock

--- src/components/DisplayBlockReverse.jsx
+++ src/components/DisplayBlockReverse.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import cellblack from "/images/cellblack.jpeg"
-import { ContainerReverse, TitleReverse, SubTitleReverse, ContentContainerReverse, ImageRight } from "../style/DisplayBlockReverseStyle"
+import {
+    ContainerReverse,
+    ContentContainerReverse,
+    TitleReverse,
+    SubTitleReverse,
+    ImageRight
+} from "../style/DisplayBlockReverseStyle"
 
 const DisplayReverse = () => {
     return (
         <ContainerReverse>
+            <ImageRight src={cellblack} />
             <ContentContainerReverse>
                 <TitleReverse>
                     Experienced and skilled developers
@@ -12,7 +19,6 @@ const DisplayReverse = () => {
                 <SubTitleReverse>
                     Our team of mobile software engineers  are highly experienced and skilled in developing mobile aplications for both iOS and Android plataforms. They have a deep understandig of the latest technologies and best pratices in mobile app development.
                 </SubTitleReverse>
-                <ImageRight src={cellblack} />
             </ContentContainerReverse>
         </ContainerReverse>
     );
